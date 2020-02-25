a84cd549833ff95e114528da53d7903b9f6e36a0
hoang <lqhoang11394@gmail.com>
2020-02-25 10:22:35 -0600

    update  4

--- routes/web.php
+++ routes/web.php
@@ -114,10 +114,10 @@ Route::group(['middleware' => ['cors','web']], function () {
 
     Route::any('api/admin/staff-listpayment', 'System\EmployeeManageController@showCommissionTypeOfStaff')  ;
     Route::any('api/admin/staff-listcommission', 'System\EmployeeManageController@showPaymentTypeOfStaff')  ;
-    Route::any('api/admin/employees', 'System\EmployeeManageController@getAllEmployeeFromVendor')  ;
+    Route::any('api/admin/all-employees', 'System\EmployeeManageController@getAllEmployeeFromVendor')  ;
 
     //services
-    Route::any('api/admin/services', 'System\EmployeeManageController@getAllServicesByVendor')  ;
+    Route::any('api/admin/all-services', 'System\EmployeeManageController@getAllServicesByVendor')  ;
     Route::any('api/admin/services', 'System\EmployeeManageController@addServices')  ;
     Route::any('api/admin/user', 'System\EmployeeManageController@getAllEmployeeFromVendor')  ;
 
