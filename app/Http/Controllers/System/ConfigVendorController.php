92fada34f7e9f5685027051c3a344fe28dc93a2a
thelashsystem@gmail.com <Bestlifescout@1>
2020-08-06 12:33:13 +0700

    update new function1

--- app/Http/Controllers/System/ConfigVendorController.php
+++ app/Http/Controllers/System/ConfigVendorController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers\System;
 use App\Http\Controllers\Controller;
 use App\Lib\MyUtils;
 use App\Model\ConfigVendorModel;
+use App\Model\Vendor;
 use Illuminate\Http\Request;
 
 class ConfigVendorController extends Controller
@@ -14,10 +15,12 @@ class ConfigVendorController extends Controller
     protected $util;
     protected $ConfigModel;
     protected $Vendor_id = 1;
+    protected $Vendor;
 
     function __construct(Request $request){
         $this->util = new MyUtils();
         $this->ConfigModel = new ConfigVendorModel();
+        $this->Vendor = new Vendor();
     }
 
     function getClientBannerConfig()
@@ -35,10 +38,27 @@ class ConfigVendorController extends Controller
     function setVendorConfig(Request $request){
         $content = $request->all();
         $data = $this->ConfigModel->setConfig($this->Vendor_id,$content['data']);
-
+        $jsonContent = json_decode($content['data'],true);
+        $InfoStore = $jsonContent['information'];
+        $this->Vendor->updateVendorInfo($this->Vendor_id,$InfoStore['store-name']['value'],$InfoStore['store-address']['value'],$InfoStore['store-phone']['value']);
+        $this->Vendor->updateVendorFinancialInfo($InfoStore['store-tax']['value'],$this->Vendor_id);
         return  $this->util->returnHttps('',0,'') ;
+    }
+
+    function getOpenHourConfig(){
+      $data = $this->ConfigModel->getOpenhours($this->Vendor_id);
+        return $this->util->returnHttps($data[0]['content'],0,'') ;
 
     }
+    function setOpenHourConfig(Request $request){
+        $content = $request->all();
+
+        $saveOpenhours = $this->ConfigModel->setOpenHours($this->Vendor_id,$content['data']);
+
+        return $this->util->returnHttps('',0,'');
+    }
+
+
 
 
 
--- app/Http/Controllers/System/VendorController.php
+++ app/Http/Controllers/System/VendorController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\System;
 use App\Http\Controllers\Controller;
 use App\Lib\MyUtils;
 use App\Lib\SMSTwillo;
+use App\Model\ConfigVendorModel;
 use App\Model\Customer;
 use App\Model\GroupService;
 use App\Model\InternalTransaction;
@@ -15,6 +16,7 @@ use App\Model\ServiceWaitlistModel;
 use App\Model\Transaction;
 use App\Model\UserAdmin;
 use Illuminate\Http\Request;
+use App\Model\Vendor;
 
 
 class VendorController
@@ -23,17 +25,24 @@ class VendorController
     protected $vendorId = 1;
     protected $customerModel;
     protected $util;
+    protected $vendor;
+    protected $vendorConfig;
 
     function __construct(Request $request){
         $this->util = new MyUtils();
-
+        $this->vendor = new Vendor();
+        $this->vendorConfig = new ConfigVendorModel();
     }
 
     function updateVendorInformation(){
-
+    $data = $this->vendorConfig->getConfig($this->vendorId);
+    $object = json_decode($data['data'],true);
+//    $this->vendor->updateVendorInfo($)
+        return $data;
     }
+
     function getVendorInfo(){
-        
+
     }
 
 }
